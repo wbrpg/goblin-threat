4753a4e23819bf64111c4fbeae8a1564c8318731
yijiahe <yi.jiahe00@gmail.com>
2023-10-11 07:14:08 +0000

    Add method to get map position from click

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,9 +1,12 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
+type Point2D = [number, number];
+type MapCoordinates = [number, number]
+
 interface MapProps {
     imageUrl: string,
     mapSize: [number, number]
-    mapPosition: [number, number]
+    mapPosition: MapCoordinates
 }
 
 function Map({imageUrl, mapSize, mapPosition}: MapProps) {
@@ -11,7 +14,7 @@ function Map({imageUrl, mapSize, mapPosition}: MapProps) {
 
     const ref = useRef<HTMLDivElement>(null);
     const [viewportShape, setViewportShape] = useState<[number, number]>([0, 0]);
-    const [backgroundPosition, setBackgroundPosition] = useState('0px 0px')
+    const [mapOffset, setMapOffset] = useState<[number, number]>([0, 0]);
 
     useEffect(() => {
         const storeViewportShape = () => {
@@ -25,32 +28,49 @@ function Map({imageUrl, mapSize, mapPosition}: MapProps) {
         window.addEventListener('resize', storeViewportShape)
     }, [])
 
-    const centreOnCurrentPosition = useCallback((mapPosition: [number, number]) => {
-        const offsets = mapPosition.map((e, i) => {
+    const centreOnCurrentPosition = useCallback((mapPosition: MapCoordinates) => {
+        setMapOffset(mapPosition.map((e, i) => {
             // offset required to place the first tile in the centre translated by the offset required to put the i-th tile in the centre. 1-indexed
             return (viewportShape[i] - tileSizePx)/2 - (e - 1)*tileSizePx;
-        });
-
-        setBackgroundPosition(`${offsets[0]}px ${offsets[1]}px`)
+        }) as [number, number]);
     }, [viewportShape])
     
     useEffect(() => {
         centreOnCurrentPosition(mapPosition);
     }, [centreOnCurrentPosition, mapPosition])
 
+    const positionToMapPosition = (position: Point2D): MapCoordinates | null => {
+        const mapPosition = position.map((e, i) => {
+            const x = Math.floor((e - mapOffset[i])/tileSizePx) + 1;
+            // If the i-th position is out-of-bounds, set to null
+            if ((x <= 0) || (x > mapSize[i])) { return null; }
+            return x;
+        });
+        
+        // If any value is null, return null 
+        if (mapPosition.some(e => e === null)) { return null; }
+
+        return mapPosition as MapCoordinates;
+    };
+
     return (
-        <div style={{
-            width: '100vw', 
-            overflow: 'hidden',
-            backgroundColor: 'gray',
+        <div
+            ref={ref}
+            style={{
+                width: '100vw', 
+                overflow: 'hidden',
+                backgroundColor: 'gray',
+            }}
+            onClick={(e) => {
+                console.log("click", e.clientX, e.clientY);
+                console.log("position", positionToMapPosition([e.clientX, e.clientY]));
             }}>
             <div 
-                ref={ref}
                 style={{
                     width: '100vw', aspectRatio: '1',
                     backgroundImage: `url('${imageUrl}')`, 
                     backgroundSize:  `${mapSize[0] * tileSizePx}px`, 
-                    backgroundPosition: backgroundPosition,
+                    backgroundPosition: `${mapOffset[0]}px ${mapOffset[1]}px`,
                     backgroundRepeat: 'no-repeat',
                 }}
             />
